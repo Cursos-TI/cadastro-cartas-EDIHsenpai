065062f4367aec53b998fbe5f041a3b8b8f21adb
EDIHsenpai <edigarsoares235@gmail.com>
2026-03-24 22:09:18 +0000

    adicionei Área para definição das variáveis.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,7 +6,12 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
-
+  /*CARTA 1, ESTADO A, CODIGO A010*/
+  char nome da cidade[9] = "São Paulo";
+  int População = 12325000;
+  float Área = 1521.11;
+  float PIB = 699.28;
+  int Número de pontos turisticos = 50;
   // Área para entrada de dados
 
   // Área para exibição dos dados da cidade
