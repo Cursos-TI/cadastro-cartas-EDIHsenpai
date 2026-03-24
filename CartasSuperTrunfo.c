efdeee7a792a19b34831bf430269ee0b669dbd45
EDIHsenpai <edigarsoares235@gmail.com>
2026-03-24 22:40:44 +0000

    falta SCANF na area de entrada de dados!

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,13 +7,17 @@
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
   /*CARTA 1, ESTADO A, CODIGO A010*/
-  char nome da cidade[9] = "São Paulo";
+  char cidade[9] = "São Paulo";
   int População = 12325000;
   float Área = 1521.11;
   float PIB = 699.28;
-  int Número de pontos turisticos = 50;
+  int Túristicos = 50;
   // Área para entrada de dados
-
+  printf("Cidade: %s\n", cidade);
+  printf("População: %d\n", População);
+  printf("Área: %d\n", Área);
+  printf("PIB: %d\n", PIB);
+  printf("Pontos turisticos: %d\n",Túristicos);
   // Área para exibição dos dados da cidade
 
 return 0;
